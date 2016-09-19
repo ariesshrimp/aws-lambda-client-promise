6d523b0c64f589332793abf151e8307f0eef3a3d
joefraley <jose.fraley@gmail.com>
2016-09-18 18:22:03 -0700

    convert to promise structure.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,42 @@
 import AWS from 'aws-sdk'
 
-/**
-* Find region @ https://console.aws.amazon.com/
-*   1. Listed in the top-right corner between your User Name and "Support" drop-downs
-*/
-// AWS.config.region = 'us-east-1'
+// All of this data with change for your application / lambda function.
+// This technique is elaborated on here:
+// http://lg.io/2015/05/16/the-future-is-now-and-its-using-aws-lambda.html
+const Lambda = new AWS.Lambda({
+  region: 'us-east-1',
+  accessKeyId: 'AKIAJMIVROLTFJRCFCQQ',
+  secretAccessKey: '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'
+})
 
-/**
-* Find AccountId @ https://console.aws.amazon.com/billing/home?#/account
-* Find RoleArn & IdentityPoolId @ https://console.aws.amazon.com/cognito/users/?[YOUR REGION HERE]]#/pool/[ YOUR POOL ID HERE]/
-*   1. Go to https://console.aws.amazon.com/cognito/
-*   2. Click "Manage your User Pools"
-*   3. Click the name of your User Pool
-*   4. Pool Id & Pool ARN are listed at the top of the page under "Pool details"
-*/
-// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-//   IdentityPoolId: 'us-east-1_a0Evrdtee'
-// })
-// AWS.config.update({
-//   accessKeyId: 'anything',
-//   secretAccessKey: 'anything'
-// })
+const invokeLambdaFunction = (payload) => {
+  let json
+  try { json = JSON.stringify(payload)}
+  catch(error) { throw new Error('The payload you gave me cant be serialized for lambda use...I need something I can JSON.stringify')}
 
-const invokeLambdaFunction = () => {
-  // Tell the SDK you're trying to talk with a Lambda service
-  const Lambda = new AWS.Lambda({
-    region: 'us-east-1',
-    accessKeyId: 'AKIAJMIVROLTFJRCFCQQ',
-    secretAccessKey: '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'
-  })
-
-  // Find FunctionName under "Functions" @ https://console.aws.amazon.com/lambda/
-  // Payload = a JSON string that your lambda function will be dealing with.
-  // Probably user params from a search input or data from a form submit or something
   const lambdaParams = {
     FunctionName: 'weather-map-API',
-    Payload: JSON.stringify({key1: 'value1'})
+    Payload: json
   }
 
-  // This is what happens when your client gets a response from your Lambda function
-  const lambdaCallback = (error, response) => {
-      return response
+  const lambdaPromise = params => {
+    return new Promise((resolve, reject) => {
+      // This callback definition is important.
+      // This is what allows us to unwrap the error-first callback
+      // and invert the control back to the promise chain
+      const lambdaCallback = (error, results) => {
+        if (error) reject(error)
+        resolve(results)
+      }
+
+      Lambda.invoke(params, lambdaCallback)
+    })
   }
 
-  // Get it done!
-  Lambda.invoke(lambdaParams, lambdaCallback)
+  // Just return the raw promise and let consumers decide what to do with the results
+  return lambdaPromise(lambdaParams)
 }
 
-invokeLambdaFunction()
+invokeLambdaFunction(0)
+  .then((response) => console.log(response))
+  .catch(error => console.error(error))
